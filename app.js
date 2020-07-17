fc1a6092a1589662d70a417c5b8d798832ac8907
achman1729 <achin.madot@gmail.com>
2020-07-17 20:02:22 +1000

    fixed errors by reinstalling app

--- app.js
+++ app.js
@@ -1,37 +1,21 @@
-var createError = require("http-errors")
-var express = require("express")
-var path = require("path")
-var cookieParser = require("cookie-parser")
-var logger = require("morgan")
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
 
 var indexRouter = require("./routes/index")
 var stocksRouter = require("./routes/stock")
 
-var app = express()
+var app = express();
 
-app.use(logger("dev"))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, "public")))
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/", indexRouter)
 app.use("/api/stocks", stocksRouter)
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404))
-})
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get("env") === "development" ? err : {}
-
-  // render the error page
-  res.status(err.status || 500)
-  res.render("error")
-})
-
-module.exports = app
+module.exports = app;
