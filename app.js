3037fc7f6fd122f63df2d56fe53455bcdf29ee9b
achman1729 <achin.madot@gmail.com>
2020-07-27 12:23:59 +1000

    cleaned app.js

--- app.js
+++ app.js
@@ -13,11 +13,6 @@ var stocksRouter = require("./routes/stock")
 var app = express()
 
 app.use(cors())
-// app.use(
-//     bodyParser.urlencoded({
-//       extended: false
-//     })
-//   )
 
 const mongoURI=`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.izu4g.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
 
